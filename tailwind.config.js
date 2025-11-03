5863ac7a49e99e2d82563c9636c46a9a522af356
aishaksl <aisha.koseoglu@gmail.com>
2025-11-03 17:05:23 +0700

    feat: extend Tailwind theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+          300: "#9ca4ab",
+        },
+        accent: "#ab8bff",
+      },
+    },
   },
   plugins: [],
 };
