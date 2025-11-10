5e02c97d10568eac2c55152b477bd0f2404c513e
aishaksl <aisha.koseoglu@gmail.com>
2025-11-10 16:41:33 +0700

    feat (learning):  add HomeScreen and onboarding components with navigation links

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-red-600" >Welcome</Text>
+      <Text className="text-dark-200" >Welcome</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avangers">Avenger Movie</Link>
     </View>
   );
 }
