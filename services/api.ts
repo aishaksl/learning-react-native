affb3cc44bf4629a79987ff88214d8e31fb02f9e
aishaksl <aisha.koseoglu@gmail.com>
2025-11-16 22:32:16 +0700

    feat: add TMDB API configuration and fetchMovies function

---
+++ services/api.ts
@@ -0,0 +1,24 @@
+export const TMDB_CONFIG = {
+BASE_URL : 'https://api.themoviedb.org/3',
+API_KEY: process.env.TMDB_API_KEY,
+headers:{
+     accept: 'application/json',
+     authorization: `Bearer ${process.env.TMDB_API_KEY}`
+   }
+}
+
+export const fetchMovies= async ({query}:{query:string})=>{
+    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`:`discover/movie?sort_by=popularity.desc`
+    const response = await fetch (endpoint,{
+        method:'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok){
+      throw new Error('Failed to fetch movies', response.statusText)
+    }
+
+    const data = await response.json();
+    return data.results
+}
+
